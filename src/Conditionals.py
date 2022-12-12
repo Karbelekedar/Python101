063ef9f04efb740588a17c427e96d46ef08ee102
Karbelekedar <karbelekedar84@gmail.com>
2022-12-12 17:36:27 +0530

    elif ladder

--- src/Conditionals.py
+++ src/Conditionals.py
@@ -12,6 +12,10 @@ Whatever is written outside else or Indented outside else is executed thereafter
 
 if weather_condition == "Rainy" or weather_condition == "Stormy":
     print("Hey, Please don't go out today.")
+elif weather_condition == "Cloudy":
+    print("Hey, you can go out today but Please carry an umbrella")
 else:
     print("Hey, you can go out today and enjoy!!")
 print("The program jumped here after the IF statement was True")
+
+
