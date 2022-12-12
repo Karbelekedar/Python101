ba5b2d5365287763dd396ec1a288fa80f6ebc837
Karbelekedar <karbelekedar84@gmail.com>
2022-12-12 16:36:44 +0530

    Added info on variables

---
+++ src/variables.py
@@ -0,0 +1,35 @@
+'''
+Variables in Python:
+
+Python is a weakly typed language and no need to specify the data type
+'''
+
+# We follow snake_case in Python
+
+x = 10
+print(x)
+
+x = "Hello World"
+print(x)
+
+a = 5
+b = 2
+c = a/b
+print(c)
+
+# Characters are specified by ''
+
+t = 'a'
+print(t)
+
+# Single and multi_line_strings
+
+name = "Kedar"
+print("My name is",name)
+
+multi_line_string = """
+Hello World
+This is Kedar
+"""
+
+print(multi_line_string)
