94cdee9ef382f4da31dd60a530dedec59d2c19ac
Karbelekedar <karbelekedar84@gmail.com>
2022-12-12 17:47:56 +0530

    For_loops

---
+++ src/For_loop.py
@@ -0,0 +1,17 @@
+'''
+FOR loops:
+Here the end point in the range isn't included
+
+'''
+
+print("Increment Loop")
+for i in range(1, 11, 1):
+    print(i, end=" ")
+
+print("")
+
+print("Decrement Loop")
+for i in range(10, 0, -1):
+    print(i, end=" ")
+
+print("")
