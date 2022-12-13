440f97271bc19df29f36280eac56affc84c2a992
Karbelekedar <karbelekedar84@gmail.com>
2022-12-13 11:36:20 +0530

    Pyramid Pattern

--- src/Pattern.py
+++ src/Pattern.py
@@ -21,3 +21,23 @@ for i in range(0, n):
         else:
             print(" ", end="")
     print(" ")
+
+'''
+Question: 
+            *
+           * *
+          * * *
+'''
+
+print("Enter number of Lines: ")
+m = int(input())
+
+
+for a in range(0, m):
+    for b in range(0, 2*m):
+        if 3-a <= b <= 3+a:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print("")
+
