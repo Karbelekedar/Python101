ff1c0d99f9f2ab4c1c574c3f70e47f2f0bea60d2
Karbelekedar <karbelekedar84@gmail.com>
2022-12-13 21:30:04 +0530

    Deconstructors

--- src/oops_1.py
+++ src/oops_1.py
@@ -1,5 +1,9 @@
 # OOPS
+'''
+During Garbage Collection, the del method is called but if we manually call the del method, it does not
+destroy the object from the memory.
 
+'''
 # Creating a class Dog and it's methods
 class Dog():
 
@@ -15,14 +19,30 @@ class Dog():
     def run(self):
         print(self.name,"is running!!")
 
+    def born(self):
+        print(self.name,"is born!!")
+
 # Creating Objects of class Dog
-obj1 = Dog()
-obj2 = Dog()
+# obj1 = Dog()
+# obj2 = Dog()
+#
+# obj1.set_name("Tommy")
+# obj2.set_name("Max")
+#
+# obj1.bark()
+# obj1.run()
+# obj2.bark()
+# obj2.run()
 
-obj1.set_name("Tommy")
-obj2.set_name("Max")
+print("Tommy is having a baby, is it a boy or a girl??")
 
-obj1.bark()
-obj1.run()
-obj2.bark()
-obj2.run()
+x = input()
+
+if x=="Boy":
+    Max = Dog("Max")
+    Max.born()
+elif x=="Girl":
+    Nina = Dog("Nina")
+    Nina.born()
+else:
+    print("Incorrect Input")
